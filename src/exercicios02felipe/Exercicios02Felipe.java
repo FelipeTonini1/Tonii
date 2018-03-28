38688bb7bb342e0181ddcd86c82684e0511f691b
IFSC <IFSC@10.135.213.118>
2018-03-28 20:04:34 -0300

    Criação da classe vetor

--- src/exercicios02felipe/Exercicios02Felipe.java
+++ src/exercicios02felipe/Exercicios02Felipe.java
@@ -15,7 +15,5 @@ public class Exercicios02Felipe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
-}
---
+++ src/exercicios02felipe/Vetor.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios02felipe;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Vetor {
+    
+            
+        
+        /* Construtor */
+        public Vetor(int size){}
+            int vet[];
+            public Vetor(int size){
+                vet= new int[size];
+            }
+        
+        /* Preenche o vetor com valores aleatórios*/
+         public void randomFill(){}
+         
+        /* Implementa o método de ordenação bubbleSort*/
+        public void bubbleSort(){}
+        
+        /* Converte o vetor para String*/
+        public String toString(){
+            return "s";
+        }
+        
+        /*  Compara se dois vetores são iguais */
+        public boolean equals(int[] v){
+            return false;
+        }
+        
+        /* Atribui um valor na posição pos do vetor */  
+        public void setAt(int pos, int value){}
+        
+        /* Retorna o valor na posição pos do vetor. */
+        public int valueAt(int pos){
+            return 0;
+        }
+        
+        /* Retorna o tamanho do vetor */
+        public int length(){
+            return 0;
+        }
+        
+        /* Concatena dois vetores */
+        public Vetor Concat(Vetor v){
+            return null;
+        }
+    
+    
+
+}
